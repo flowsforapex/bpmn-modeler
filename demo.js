7a2c171bcb99b72410f84eee0ae68ebd472348f7
Senger, Alexander <alexander.senger@mt-ag.com>
2020-10-15 16:14:27 +0200

    Linting dev integration

--- demo.js
+++ demo.js
@@ -5,7 +5,7 @@ import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/bpmn
 
 
 import lintModule from 'bpmn-js-bpmnlint';
-import bpmnlintConfig from 'bpmnlint-Rules/.bpmnlintrc';
+import bpmnlintConfig from './.bpmnlintrc';
 
 
 async function init() {
@@ -16,8 +16,7 @@ async function init() {
       parent: '#properties'
     },
     linting: {
-      bpmnlint: bpmnlintConfig,
-      active: getUrlParam('linting')
+      bpmnlint: bpmnlintConfig
     },
     additionalModules: [
       propertiesPanelModule,
@@ -30,14 +29,14 @@ async function init() {
 
   async function openDiagram() {
     const defaultDiagram =
-    "<?xml version='1.0' encoding='UTF-8'?>" +
-    "<bpmn:definitions xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:bpmn='http://www.omg.org/spec/BPMN/20100524/MODEL' xmlns:bpmndi='http://www.omg.org/spec/BPMN/20100524/DI' id='Definitions_1wzb475' targetNamespace='http://bpmn.io/schema/bpmn' exporter='bpmn-js (https://demo.bpmn.io)' exporterVersion='7.2.0'>" +
-    "<bpmn:process id='Process_0rxermh' isExecutable='false' />" +
-    "<bpmndi:BPMNDiagram id='BPMNDiagram_1'>" +
-    "<bpmndi:BPMNPlane id='BPMNPlane_1' bpmnElement='Process_0rxermh' />" +
-    "</bpmndi:BPMNDiagram>" +
-    "</bpmn:definitions>";
-    
+      "<?xml version='1.0' encoding='UTF-8'?>" +
+      "<bpmn:definitions xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:bpmn='http://www.omg.org/spec/BPMN/20100524/MODEL' xmlns:bpmndi='http://www.omg.org/spec/BPMN/20100524/DI' id='Definitions_1wzb475' targetNamespace='http://bpmn.io/schema/bpmn' exporter='bpmn-js (https://demo.bpmn.io)' exporterVersion='7.2.0'>" +
+      "<bpmn:process id='Process_0rxermh' isExecutable='false' />" +
+      "<bpmndi:BPMNDiagram id='BPMNDiagram_1'>" +
+      "<bpmndi:BPMNPlane id='BPMNPlane_1' bpmnElement='Process_0rxermh' />" +
+      "</bpmndi:BPMNDiagram>" +
+      "</bpmn:definitions>";
+
     // import diagram
     try {
       await bpmnModeler.importXML(defaultDiagram);
@@ -53,34 +52,5 @@ async function init() {
     }
   }
 
-
-bpmnModeler.on('linting.toggle', function(event) {
-
-  var active = event.active;
-
-  setUrlParam('linting', active);
-});
-
-
-// helpers /////////////////////////////////
-
-function setUrlParam(name, value) {
-
-  var url = new URL(window.location.href);
-
-  if (value) {
-    url.searchParams.set(name, 1);
-  } else {
-    url.searchParams.delete(name);
-  }
-
-  window.history.replaceState({}, null, url.href);
-}
-
-function getUrlParam(name) {
-  var url = new URL(window.location.href);
-
-  return url.searchParams.has(name);
-}
 }
 init();
