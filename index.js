2277f7f845019345cdfcb34353150f307b7b6fa1
commi235 <moritz.klein@it-twins.de>
2020-10-27 10:12:53 +0100

    include new PropertiesProvider in Bundle

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 import BpmnModeler from "bpmn-js/lib/Modeler";
 
 import propertiesPanelModule from "bpmn-js-properties-panel";
-import propertiesProviderModule from "bpmn-js-properties-panel/lib/provider/bpmn";
+import propertiesProviderModule from "./apexPropertiesProvider/provider";
+import apexModdleDescriptor from "./apexPropertiesProvider/descriptor/apexProps";
 
 var bpmnModeler = {
   Modeler: BpmnModeler,
   modules: {
     propertiesPanelModule,
     propertiesProviderModule
+  },
+  moddleExtensions: {
+    apexModdleDescriptor
   }
 };
 
