0a0c6ca477982f118efc8570ff2a5f868cdbf2a6
phartenfeller <pharti@hotmail.de>
2020-11-13 16:46:36 +0100

    upgraded webpack and added terser as minifier

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,28 +1,32 @@
 const CopyPlugin = require("copy-webpack-plugin");
 const path = require("path");
+const TerserJSPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-    entry: {
-      "mtag.bpmnmodeler": [path.resolve(__dirname, "index.js")],
-    },
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: '[name].min.js',
-      library: "bpmnModeler",
-      libraryTarget: "var",
-      libraryExport: "default"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.bpmnlintrc$/,
-          use: [
-            {
-              loader: 'bpmnlint-loader',
-            }
-          ]
-        }
-      ]
-    },
-  }
-;
+  entry: {
+    "mtag.bpmnmodeler": [path.resolve(__dirname, "index.js")],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].min.js",
+    library: "bpmnModeler",
+    libraryTarget: "var",
+    libraryExport: "default",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.bpmnlintrc$/,
+        use: [
+          {
+            loader: "bpmnlint-loader",
+          },
+        ],
+      },
+    ],
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserJSPlugin()],
+  },
+};
