5ef723dc38f088a732395437ec2e5a1bc49d5fc9
StephanRamke <stephan.ramke@mt-ag.com>
2021-05-07 15:55:46 +0200

    Temporary enable camunda extensions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import BpmnModeler from 'bpmn-js/lib/Modeler';
 import propertiesPanelModule from 'bpmn-js-properties-panel';
 import propertiesProviderModule from './apexPropertiesProvider/provider';
 import apexModdleDescriptor from './apexPropertiesProvider/descriptor/apexProps';
+import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
+import camundaModule from 'bpmn-js-properties-panel/lib/provider/camunda';
 
 import lintModule from 'bpmn-js-bpmnlint';
 import bpmnlintConfig from './.bpmnlintrc'; 
@@ -12,11 +14,13 @@ var bpmnModeler = {
   linting: { apexLinting: bpmnlintConfig },
   customModules: {
     propertiesPanelModule,
+    camundaModule,
     propertiesProviderModule,
     lintModule
   },
   moddleExtensions: {
-    apexModdleDescriptor
+    apexModdleDescriptor,
+    camundaModdleDescriptor
   }
 };
 
