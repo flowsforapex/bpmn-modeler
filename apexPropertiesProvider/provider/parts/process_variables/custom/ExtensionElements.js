75b78d43b673de91bd859a9380726a88c0d6074d
damthor <dennis.amthor@mt-ag.com>
2021-08-02 15:13:59 +0200

    Fixed icon alignment, changed to font apex

--- apexPropertiesProvider/provider/parts/process_variables/custom/ExtensionElements.js
+++ apexPropertiesProvider/provider/parts/process_variables/custom/ExtensionElements.js
@@ -1,20 +1,21 @@
-'use strict';
 
-var getBusinessObject = require('bpmn-js/lib/util/ModelUtil').getBusinessObject;
 
-var domQuery = require('min-dom').query,
-    domClosest = require('min-dom').closest,
-    domify = require('min-dom').domify,
-    forEach = require('lodash/forEach');
+var {getBusinessObject} = require('bpmn-js/lib/util/ModelUtil');
 
-var elementHelper = require('bpmn-js-properties-panel/lib/helper/ElementHelper'),
-    cmdHelper = require('bpmn-js-properties-panel/lib/helper/CmdHelper'),
-    utils = require('bpmn-js-properties-panel/lib/Utils'),
-    escapeHTML = utils.escapeHTML;
+var domQuery = require('min-dom').query;
+    var domClosest = require('min-dom').closest;
+    var {domify} = require('min-dom');
+    var forEach = require('lodash/forEach');
+
+var elementHelper = require('bpmn-js-properties-panel/lib/helper/ElementHelper');
+    var cmdHelper = require('bpmn-js-properties-panel/lib/helper/CmdHelper');
+    var utils = require('bpmn-js-properties-panel/lib/Utils');
+
+    var {escapeHTML} = utils;
 
 function getSelectBox(node, id) {
   var currentTab = domClosest(node, 'div.bpp-properties-tab');
-  var query = 'select[name=selectedExtensionElement]' + (id ? '[id=cam-extensionElements-' + id + ']' : '');
+  var query = `select[name=selectedExtensionElement]${id ? `[id=cam-extensionElements-${id}]` : ''}`;
   return domQuery(query, currentTab);
 }
 
@@ -27,62 +28,62 @@ function getSelected(node, id) {
 }
 
 function generateElementId(prefix) {
-  prefix = prefix + '_';
+  prefix += '_';
   return utils.nextId(prefix);
 }
 
-var CREATE_EXTENSION_ELEMENT_ACTION = 'create-extension-element',
-    REMOVE_EXTENSION_ELEMENT_ACTION = 'remove-extension-element';
+var CREATE_EXTENSION_ELEMENT_ACTION = 'create-extension-element';
+    var REMOVE_EXTENSION_ELEMENT_ACTION = 'remove-extension-element';
 
-module.exports = function(element, bpmnFactory, options, translate) {
+module.exports = function (element, bpmnFactory, options, translate) {
 
-  var id = options.id,
-      prefix = options.prefix || 'elem',
-      label = options.label || id,
-      idGeneration = (options.idGeneration === false) ? options.idGeneration : true,
-      businessObject = options.businessObject || getBusinessObject(element),
-      type = options.type;
+  var {id} = options;
+      var prefix = options.prefix || 'elem';
+      var label = options.label || id;
+      var idGeneration = (options.idGeneration === false) ? options.idGeneration : true;
+      var businessObject = options.businessObject || getBusinessObject(element);
+      var {type} = options;
 
   var modelProperty = options.modelProperty || 'id';
 
   var getElements = options.getExtensionElements;
 
-  var createElement = options.createExtensionElement,
-      canCreate = typeof createElement === 'function';
+  var createElement = options.createExtensionElement;
+      var canCreate = typeof createElement === 'function';
 
-  var removeElement = options.removeExtensionElement,
-      canRemove = typeof removeElement === 'function';
+  var removeElement = options.removeExtensionElement;
+      var canRemove = typeof removeElement === 'function';
 
-  var onSelectionChange = options.onSelectionChange;
-  var onEntryMoved = options.onEntryMoved;
+  var {onSelectionChange} = options;
+  var {onEntryMoved} = options;
 
-  var hideElements = options.hideExtensionElements,
-      canBeHidden = typeof hideElements === 'function';
+  var hideElements = options.hideExtensionElements;
+      var canBeHidden = typeof hideElements === 'function';
 
-  var setOptionLabelValue = options.setOptionLabelValue;
+  var {setOptionLabelValue} = options;
 
-  var defaultSize = options.size || 5,
-      resizable = options.resizable;
+  var defaultSize = options.size || 5;
+      var {resizable} = options;
 
   var reference = options.reference || undefined;
 
-  var selectionChanged = function(element, node, event, scope) {
+  var selectionChanged = function (element, node, event, scope) {
     if (typeof onSelectionChange === 'function') {
       return onSelectionChange(element, node, event, scope);
     }
   };
 
-  var entryMoved = function(element) {
+  var entryMoved = function (element) {
     if (typeof onEntryMoved === 'function') {
       return onEntryMoved(element);
     }
-  }
-
-  var createOption = function(value) {
-    return '<option value="' + escapeHTML(value) + '" data-value data-name="extensionElementValue">' + escapeHTML(value) + '</option>';
   };
 
-  var initSelectionSize = function(selectBox, optionsLength) {
+  var createOption = function (value) {
+    return `<option value="${escapeHTML(value)}" data-value data-name="extensionElementValue">${escapeHTML(value)}</option>`;
+  };
+
+  var initSelectionSize = function (selectBox, optionsLength) {
     if (resizable) {
       selectBox.size = optionsLength > defaultSize ? optionsLength : defaultSize;
     }
@@ -91,37 +92,37 @@ module.exports = function(element, bpmnFactory, options, translate) {
   return {
     id: id,
     type: type,
-    html: '<div class="bpp-row bpp-element-list" ' +
-            (canBeHidden ? 'data-show="hideElements"' : '') + '>' +
-            '<label for="cam-extensionElements-' + escapeHTML(id) + '">' + escapeHTML(label) + '</label>' +
+    html: `<div class="bpp-row bpp-element-list" ${ 
+            canBeHidden ? 'data-show="hideElements"' : ''}>` +
+            `<label for="cam-extensionElements-${escapeHTML(id)}">${escapeHTML(label)}</label>` +
             '<div class="bpp-field-wrapper">' +
-              '<select id="cam-extensionElements-' + escapeHTML(id) + '"' +
+              `<select id="cam-extensionElements-${escapeHTML(id)}"` +
                       'name="selectedExtensionElement" ' +
-                      'size="' + escapeHTML(defaultSize) + '" ' +
+                      `size="${escapeHTML(defaultSize)}" ` +
                       'data-list-entry-container ' +
                       'data-on-change="selectElement">' +
-              '</select>' +
-              (canCreate ? '<button class="action-button add" ' +
-                                   'id="cam-extensionElements-create-' + escapeHTML(id) + '" ' +
+              `</select>${ 
+              canCreate ? `${'<button class="action-button add fa fa-plus" ' +
+                                   'id="cam-extensionElements-create-'}${escapeHTML(id)}" ` +
                                    'data-action="createElement">' +
                              '<span>+</span>' +
-                           '</button>' : '') +
-              (canRemove ? '<button class="action-button clear" ' +
-                                   'id="cam-extensionElements-remove-' + escapeHTML(id) + '" ' +
+                           '</button>' : '' 
+              }${canRemove ? `${'<button class="action-button clear fa fa-remove" ' +
+                                   'id="cam-extensionElements-remove-'}${escapeHTML(id)}" ` +
                                    'data-action="removeElement" ' +
                                    'data-disable="disableRemove">' +
                              '<span>-</span>' +
-                           '</button>' : '') +
-              '<button class="action-button down" id="cam-extensionElements-down-' + escapeHTML(id) + '" data-action="moveDown" data-disable="disableMoveDown"><span>+</span></button>' +
-              '<button class="action-button up" id="cam-extensionElements-up-' + escapeHTML(id) + '" data-action="moveUp" data-disable="disableMoveUp"><span>+</span></button>' +
+                           '</button>' : '' 
+              }<button class="action-button down fa fa-arrow-down" id="cam-extensionElements-down-${escapeHTML(id)}" data-action="moveDown" data-disable="disableMoveDown"><span>d</span></button>` +
+              `<button class="action-button up fa fa-arrow-up" id="cam-extensionElements-up-${escapeHTML(id)}" data-action="moveUp" data-disable="disableMoveUp"><span>u</span></button>` +
             '</div>' +
           '</div>',
 
-    get: function(element, node) {
+    get: function (element, node) {
       var elements = getElements(element, node);
 
       var result = [];
-      forEach(elements, function(elem) {
+      forEach(elements, function (elem) {
         result.push({
           extensionElementValue: elem.get(modelProperty)
         });
@@ -133,16 +134,14 @@ module.exports = function(element, bpmnFactory, options, translate) {
       return result;
     },
 
-    set: function(element, values, node) {
+    set: function (element, values, node) {
       var action = this.__action;
       delete this.__action;
 
       businessObject = businessObject || getBusinessObject(element);
 
       var bo =
-        (reference && businessObject.get(reference))
-          ? businessObject.get(reference)
-          : businessObject;
+        (reference && businessObject.get(reference)) ? businessObject.get(reference) : businessObject;
 
       var extensionElements = bo.get('extensionElements');
 
@@ -155,28 +154,27 @@ module.exports = function(element, bpmnFactory, options, translate) {
         commands.push(createElement(element, extensionElements, action.value, node));
         return commands;
 
-      }
-      else if (action.id === REMOVE_EXTENSION_ELEMENT_ACTION) {
+      } else if (action.id === REMOVE_EXTENSION_ELEMENT_ACTION) {
         return removeElement(element, extensionElements, action.value, action.idx, node);
       }
 
     },
 
-    createListEntryTemplate: function(value, index, selectBox) {
+    createListEntryTemplate: function (value, index, selectBox) {
       initSelectionSize(selectBox, selectBox.options.length + 1);
       return createOption(value.extensionElementValue);
     },
 
-    deselect: function(element, node) {
+    deselect: function (element, node) {
       var selectBox = getSelectBox(node, id);
       selectBox.selectedIndex = -1;
     },
 
-    getSelected: function(element, node) {
+    getSelected: function (element, node) {
       return getSelected(node, id);
     },
 
-    setControlValue: function(element, node, option, property, value, idx) {
+    setControlValue: function (element, node, option, property, value, idx) {
       node.value = value;
 
       if (!setOptionLabelValue) {
@@ -186,7 +184,7 @@ module.exports = function(element, bpmnFactory, options, translate) {
       }
     },
 
-    createElement: function(element, node) {
+    createElement: function (element, node) {
 
       // create option template
       var generatedId;
@@ -215,7 +213,7 @@ module.exports = function(element, bpmnFactory, options, translate) {
       return true;
     },
 
-    removeElement: function(element, node) {
+    removeElement: function (element, node) {
       var selection = getSelected(node, id);
 
       var selectBox = getSelectBox(node, id);
@@ -233,26 +231,26 @@ module.exports = function(element, bpmnFactory, options, translate) {
       return true;
     },
 
-    hideElements: function(element, entryNode, node, scopeNode) {
+    hideElements: function (element, entryNode, node, scopeNode) {
       return !hideElements(element, entryNode, node, scopeNode);
     },
 
-    disableRemove: function(element, entryNode, node, scopeNode) {
+    disableRemove: function (element, entryNode, node, scopeNode) {
       return (getSelected(entryNode, id) || {}).idx < 0;
     },
 
     selectElement: selectionChanged,
 
-    moveUp: function(element, node) {
-        var selection = getSelected(node, id),
-            selectBox = getSelectBox(node, id),
-            elements = getElements(element, node);
+    moveUp: function (element, node) {
+        var selection = getSelected(node, id);
+            var selectBox = getSelectBox(node, id);
+            var elements = getElements(element, node);
 
-        var prevOption = selectBox.options[selection.idx-1],
-            currentOption = selectBox.options[selection.idx];
+        var prevOption = selectBox.options[selection.idx - 1];
+            var currentOption = selectBox.options[selection.idx];
 
-        var prevElement = elements[prevOption.index],
-            currentElement = elements[currentOption.index];
+        var prevElement = elements[prevOption.index];
+            var currentElement = elements[currentOption.index];
 
         elements[prevOption.index] = currentElement;
         elements[currentOption.index] = prevElement;
@@ -262,23 +260,23 @@ module.exports = function(element, bpmnFactory, options, translate) {
         entryMoved(element);
     },
 
-    disableMoveUp : function(element, node) {
-        var selection = getSelected(node, id),
-            selectBox = getSelectBox(node, id);
+    disableMoveUp: function (element, node) {
+        var selection = getSelected(node, id);
+            var selectBox = getSelectBox(node, id);
 
-        return !(selectBox && selection && selectBox.options[selection.idx-1]);
+        return !(selectBox && selection && selectBox.options[selection.idx - 1]);
     },
 
-    moveDown: function(element, node) {
-        var selection = getSelected(node, id),
-            selectBox = getSelectBox(node, id),
-            elements = getElements(element, node);
+    moveDown: function (element, node) {
+        var selection = getSelected(node, id);
+            var selectBox = getSelectBox(node, id);
+            var elements = getElements(element, node);
 
-        var nextOption = selectBox.options[selection.idx+1],
-            currentOption = selectBox.options[selection.idx];
+        var nextOption = selectBox.options[selection.idx + 1];
+            var currentOption = selectBox.options[selection.idx];
 
-        var nextElement = elements[nextOption.index],
-            currentElement = elements[currentOption.index];
+        var nextElement = elements[nextOption.index];
+            var currentElement = elements[currentOption.index];
 
         elements[nextOption.index] = currentElement;
         elements[currentOption.index] = nextElement;
@@ -288,11 +286,11 @@ module.exports = function(element, bpmnFactory, options, translate) {
         entryMoved(element);
     },
 
-    disableMoveDown : function(element, node) {
-        var selection = getSelected(node, id),
-            selectBox = getSelectBox(node, id);
+    disableMoveDown: function (element, node) {
+        var selection = getSelected(node, id);
+            var selectBox = getSelectBox(node, id);
 
-        return !(selectBox && selection && selectBox.options[selection.idx+1] && selection.idx > -1);
+        return !(selectBox && selection && selectBox.options[selection.idx + 1] && selection.idx > -1);
     }
   };
 };
