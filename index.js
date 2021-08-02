b0b87299f515f2d11db7a253feb4a951678f02d1
damthor <dennis.amthor@mt-ag.com>
2021-08-02 13:18:08 +0200

    Fixed demo, added config for esLint and Prettier

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
-import BpmnModeler from 'bpmn-js/lib/Modeler';
-
-import propertiesPanelModule from './custom/properties-panel';
-import propertiesProviderModule from './apexPropertiesProvider/provider';
-import apexModdleDescriptor from './apexPropertiesProvider/descriptor/apexProps';
-import camundaModdleDescriptor from 'camunda-bpmn-moddle/resources/camunda';
-import camundaModule from 'bpmn-js-properties-panel/lib/provider/camunda';
-
 import lintModule from 'bpmn-js-bpmnlint';
+import BpmnModeler from 'bpmn-js/lib/Modeler';
 import bpmnlintConfig from './.bpmnlintrc';
-
+import apexModdleDescriptor from './apexPropertiesProvider/descriptor/apexProps';
+import propertiesProviderModule from './apexPropertiesProvider/provider';
 import customPaletteProviderModule from './custom/palette';
+import propertiesPanelModule from './custom/properties-panel';
+
 
 var bpmnModeler = {
   Modeler: BpmnModeler,
   linting: { apexLinting: bpmnlintConfig },
   customModules: {
     propertiesPanelModule,
-    //camundaModule,
     propertiesProviderModule,
     lintModule,
     customPaletteProviderModule
   },
   moddleExtensions: {
     apexModdleDescriptor,
-    //camundaModdleDescriptor
   }
 };
 
