048a25edbb35270383545287a130ad4874276388
damthor <dennis.amthor@mt-ag.com>
2021-08-05 11:53:14 +0200

    Changed process status prop to select box

--- apexPropertiesProvider/provider/parts/events/TerminationEventDefinition.js
+++ apexPropertiesProvider/provider/parts/events/TerminationEventDefinition.js
@@ -7,58 +7,38 @@ const TimerEventDefinition = require('./TimerEventDefinition');
 
 module.exports = function(group, element, bpmnFactory, terminationEventDefinition, elementRegistry, translate) {
 
-    var getValue = function(modelProperty) {
-    return function(element) {
-        var modelPropertyValue = terminationEventDefinition.get('apex:' + modelProperty);
-        var value = {};
+    var getValue = function() {
+        return function(element) {
+            var processStatus = terminationEventDefinition.get('processStatus');
 
-        value[modelProperty] = modelPropertyValue;
-        return value;
-    };
-    };
-
-    var setValue = function(modelProperty) {
-    return function(element, values) {
-        var props = {};
-
-        props['apex:' + modelProperty] = values[modelProperty] || undefined;
-
-        return cmdHelper.updateBusinessObject(element, terminationEventDefinition, props);
-};
-  };
-
-//   group.entries.push(entryFactory.selectBox(translate, {
-//     id: 'processStatus',
-//     label: translate('Process status after termination'),
-//     modelProperty: 'processStatus',
-//     selectOptions: [
-//         { value: 'terminated', name: translate('Terminated') },
-//         { value: 'completed', name: translate('Completed') }
-//     ],
-
-//     get: getValue('processStatus'),
-//     set: setValue('processStatus')
-//   }));
-
-    group.entries.push(entryFactory.checkbox(translate, {
-        id: 'processStatus',
-        label: translate('Mark process as completed'),
-        modelProperty: 'processStatus',
-        description: 'Process will be marked as terminated by default.',
-
-        get: function(element, node) {
-            if (terminationEventDefinition.processStatus === undefined) {
-                var command = cmdHelper.updateBusinessObject(element, terminationEventDefinition, { processStatus: 'terminated' });
+            if (typeof processStatus === 'undefined') {
+                var command = cmdHelper.updateBusinessObject(element, terminationEventDefinition, { processStatus: 'completed' });
                 new UpdateBusinessObjectHandler(elementRegistry, bpmnFactory).execute(command.context);
             }
+            
             return {
-                processStatus: (terminationEventDefinition.processStatus === 'completed')
+                processStatus: processStatus || undefined
             };
-        },
+        };
+    };
 
-        set: function(element, values) {
-            values.processStatus = values.processStatus ? 'completed' : 'terminated';
-            return cmdHelper.updateBusinessObject(element, terminationEventDefinition, values);
-        }
+    var setValue = function() {
+        return function(element, values) {
+
+            return cmdHelper.updateBusinessObject(element, terminationEventDefinition, { processStatus: values['processStatus'] });
+        };
+    };
+
+    group.entries.push(entryFactory.selectBox(translate, {
+        id: 'processStatus',
+        label: translate('Process status after termination'),
+        modelProperty: 'processStatus',
+        selectOptions: [
+            { value: 'completed', name: translate('Completed') },
+            { value: 'terminated', name: translate('Terminated') }
+        ],
+
+        get: getValue('processStatus'),
+        set: setValue('processStatus')
     }));
 };
