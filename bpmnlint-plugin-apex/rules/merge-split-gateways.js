94c3ff6993f6a9155e044c3ef12a789287d2dc00
damthor <dennis.amthor@mt-ag.com>
2021-08-23 11:11:03 +0200

    Added merge-split-gateways linter rule

---
+++ bpmnlint-plugin-apex/rules/merge-split-gateways.js
@@ -0,0 +1,15 @@
+/**
+ * A rule that warns when using gateways that merge and open in the same time.
+ */
+ module.exports = function () {
+  function check(businessObject, reporter) {
+
+    if (businessObject.$type === 'bpmn:ExclusiveGateway' || businessObject.$type === 'bpmn:ParallelGateway' || businessObject.$type === 'bpmn:InclusiveGateway') {
+      if (businessObject.incoming.length > 1 && businessObject.outgoing.length > 1) {
+        reporter.report(businessObject.id, 'A gateway should not merge and split at the same time.');
+      }
+    }
+  }
+
+  return { check };
+};
