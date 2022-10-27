45f55662c5d710b2472e9918f645c0840bce6d95
Dennis Amthor <79202384+damthor@users.noreply.github.com>
2022-10-27 13:00:11 +0200

    Updated bpmn-js-properties-panel to latest version, added ConnectorsExtensionModule

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import AddExporter from '@bpmn-io/add-exporter';
 import lintModule from 'bpmn-js-bpmnlint';
+// import ConnectorsExtensionModule from 'bpmn-js-connectors-extension';
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import bpmnlintConfig from './.bpmnlintrc';
 import apexModdleDescriptor from './apexPropertiesProvider/descriptor/apexProps';
-import propertiesProviderModule from './apexPropertiesProvider/provider';
+// import propertiesProviderModule from './apexPropertiesProvider/provider';
 import customPaletteProviderModule from './custom/palette';
-import propertiesPanelModule from './custom/properties-panel';
+// import propertiesPanelModule from './custom/properties-panel';
 import styleModule from './lib/styleModule';
 import translationModule from './lib/translationModule';
 import xmlModule from './lib/xmlModule';
@@ -14,14 +15,15 @@ var bpmnModeler = {
   Modeler: BpmnModeler,
   linting: { apexLinting: bpmnlintConfig },
   customModules: {
-    propertiesPanelModule,
-    propertiesProviderModule,
+    // propertiesPanelModule,
+    // propertiesProviderModule,
     lintModule,
     customPaletteProviderModule,
     translationModule,
     styleModule,
     xmlModule,
     AddExporter,
+    // ConnectorsExtensionModule,
   },
   moddleExtensions: {
     apexModdleDescriptor,
