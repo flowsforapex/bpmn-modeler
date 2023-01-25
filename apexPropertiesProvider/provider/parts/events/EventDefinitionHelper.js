d7c20d8d4cdec7e577f8e92165d192af03d009a2
Dennis Amthor <79202384+damthor@users.noreply.github.com>
2023-01-25 14:19:55 +0100

    Removed old provider

--- apexPropertiesProvider/provider/parts/events/EventDefinitionHelper.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-var EventDefinitionHelper = require('bpmn-js-properties-panel/lib/helper/EventDefinitionHelper');
-
-EventDefinitionHelper.getTerminationEventDefinition = function(element) {
-    return this.getEventDefinition(element, 'bpmn:TerminateEventDefinition');
-};
-
-module.exports = EventDefinitionHelper;
