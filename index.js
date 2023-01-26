b04b5880c40796deb9af52a97fb56a894102d4e4
Dennis Amthor <79202384+damthor@users.noreply.github.com>
2023-01-26 11:35:32 +0100

    Cleanup, combined modules, removed unneded customization and xml migration

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import AddExporter from '@bpmn-io/add-exporter';
 import lintModule from 'bpmn-js-bpmnlint';
-// import ConnectorsExtensionModule from 'bpmn-js-connectors-extension';
 import BpmnModeler from 'bpmn-js/lib/Modeler';
 import bpmnlintConfig from './.bpmnlintrc';
 import apexModdleDescriptor from './apexPropertiesProvider/descriptor/apexProps';
 import propertiesProviderModule from './apexPropertiesProvider/new_provider';
 import customPaletteProviderModule from './custom/palette';
-// import propertiesPanelModule from './custom/properties-panel';
-import styleModule from './lib/styleModule';
 import translationModule from './lib/translationModule';
 import xmlModule from './lib/xmlModule';
 
@@ -22,15 +19,12 @@ var bpmnModeler = {
   customModules: {
     BpmnPropertiesPanelModule,
     BpmnPropertiesProviderModule,
-    // propertiesPanelModule,
     propertiesProviderModule,
     lintModule,
     customPaletteProviderModule,
     translationModule,
-    styleModule,
     xmlModule,
     AddExporter,
-    // ConnectorsExtensionModule,
   },
   moddleExtensions: {
     apexModdleDescriptor,
