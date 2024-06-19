78cb35e5ecb3780213686b5005ef8fb050b97805
Dennis Amthor <dennis.amthor@mt-ag.com>
2024-06-19 15:33:33 +0200

    Updated simpleForm properties

--- apexPropertiesProvider/new_provider/parts/userTask/SimpleFormProps.js
+++ apexPropertiesProvider/new_provider/parts/userTask/SimpleFormProps.js
@@ -1,16 +1,16 @@
-import { isSelectEntryEdited, isTextAreaEntryEdited } from '@bpmn-io/properties-panel';
+import { isSelectEntryEdited, isTextAreaEntryEdited, isTextFieldEntryEdited } from '@bpmn-io/properties-panel';
 import { useService } from 'bpmn-js-properties-panel';
 
 import { getBusinessObject } from '../../helper/util';
 
 import ExtensionHelper from '../../helper/ExtensionHelper';
 
-import { DefaultSelectEntryAsync, DefaultTextAreaEntryWithEditor, DefaultToggleSwitchEntry } from '../../helper/templates';
+import { DefaultSelectEntryAsync, DefaultTextAreaEntryWithEditor, DefaultToggleSwitchEntry, DefaultTextFieldEntry } from '../../helper/templates';
 
 import { useEffect, useState } from '@bpmn-io/properties-panel/preact/hooks';
 import { html } from 'htm/preact';
 
-import { getFormTemplates } from '../../plugins/metaDataCollector';
+import { getFormTemplates, getApplications, getPages, getItems } from '../../plugins/metaDataCollector';
 
 const extensionHelper = new ExtensionHelper('apex:ApexSimpleForm');
 
@@ -51,6 +51,33 @@ export default function (args) {
     if (manualInput) {
       entries.push(
         {
+          id: 'applicationIdText',
+          element,
+          label: translate('Application ID'),
+          helper: extensionHelper,
+          property: 'applicationId',
+          component: DefaultTextFieldEntry,
+          isEdited: isTextFieldEntryEdited,
+        },
+        {
+          id: 'pageIdText',
+          element,
+          label: translate('Page ID'),
+          helper: extensionHelper,
+          property: 'pageId',
+          component: DefaultTextFieldEntry,
+          isEdited: isTextFieldEntryEdited,
+        },
+        {
+          id: 'itemNameText',
+          element,
+          label: translate('Item Name'),
+          helper: extensionHelper,
+          property: 'itemName',
+          component: DefaultTextFieldEntry,
+          isEdited: isTextFieldEntryEdited,
+        },
+        {
           id: 'formTemplate',
           element,
           label: translate('Form Template'),
@@ -64,11 +91,29 @@ export default function (args) {
     } else {
       entries.push(
         {
+          id: 'applicationId',
+          element,
+          component: ApplicationProp,
+          isEdited: isSelectEntryEdited
+        },
+        {
+          id: 'pageId',
+          element,
+          component: PageProp,
+          isEdited: isSelectEntryEdited,
+        },
+        {
+          id: 'itemName',
+          element,
+          component: ItemNameProp,
+          isEdited: isSelectEntryEdited,
+        },
+        {
           id: 'formTemplateId',
           element,
           component: FormTemplateProp,
           isEdited: isSelectEntryEdited,
-        }
+        },
       );
     }
   }
@@ -94,5 +139,81 @@ function FormTemplateProp(props) {
     helper=${extensionHelper}
     property=formTemplateId
     state=${formTemplates}
+  />`;
+}
+function ApplicationProp(props) {
+
+  const {element, id} = props;
+
+  const translate = useService('translate');
+
+  const [applications, setApplications] = useState({});
+
+  useEffect(() => {
+    getApplications().then(a => setApplications({ values: a, loaded: true }));
+  }, []);
+
+  return html`<${DefaultSelectEntryAsync}
+    id=${id}
+    element=${element}
+    label=${translate('Application')}
+    helper=${extensionHelper}
+    property=applicationId
+    state=${applications}
+  />`;
+}
+
+function PageProp(props) {
+
+  const {element, id} = props;
+
+  const translate = useService('translate');
+
+  const [pages, setPages] = useState({});
+
+  const applicationId = extensionHelper.getExtensionProperty(element, 'applicationId');
+
+  useEffect(() => {
+    getPages(applicationId).then(p => setPages({ values: p, loaded: true, applicationId: applicationId }));
+  }, [applicationId]);
+
+  const needsRefresh = applicationId !== pages.applicationId;
+
+  return html`<${DefaultSelectEntryAsync}
+    id=${id}
+    element=${element}
+    label=${translate('Page')}
+    helper=${extensionHelper}
+    property=pageId
+    state=${pages}
+    needsRefresh=${needsRefresh}
+  />`;
+}
+
+function ItemNameProp(props) {
+
+  const {element, id} = props;
+
+  const translate = useService('translate');
+
+  const [items, setItems] = useState({});
+
+  const applicationId = extensionHelper.getExtensionProperty(element, 'applicationId');
+  const pageId = extensionHelper.getExtensionProperty(element, 'pageId');
+
+  useEffect(() => {
+    getItems(applicationId, pageId).then(i => setItems({ values: i, loaded: true, applicationId: applicationId, pageId: pageId }));
+  }, [applicationId, pageId]);
+
+  const needsRefresh = (applicationId !== items.applicationId) || (pageId !== items.pageId);
+
+  return html`<${DefaultSelectEntryAsync}
+    id=${id}
+    element=${element}
+    label=${translate('Item')}
+    helper=${extensionHelper}
+    property=itemName
+    state=${items}
+    needsRefresh=${needsRefresh}
   />`;
 }
