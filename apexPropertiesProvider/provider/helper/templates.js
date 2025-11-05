a27b4689c86f9d4abd199fc0318e37e162bda5f5
Dennis Amthor <dennis.amthor@mt-ag.com>
2025-11-05 14:28:20 +0100

    Removed unneded code/fixed double execution

--- apexPropertiesProvider/provider/helper/templates.js
+++ apexPropertiesProvider/provider/helper/templates.js
@@ -205,7 +205,7 @@ export function DefaultSelectEntryAsync(props) {
 }
 
 export function DefaultToggleSwitchEntry(props) {
-  const { id, element, listElement, label, description, helper, property, defaultValue, invert, cleanup, cleanupHelper } = props;
+  const { id, element, listElement, label, description, helper, property, defaultValue, invert, cleanup } = props;
 
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
@@ -242,24 +242,12 @@ export function DefaultToggleSwitchEntry(props) {
       helper.setExtensionProperty(element, modeling, bpmnFactory, {
         // eslint-disable-next-line no-nested-ternary
         [property]: value ? (invert ? 'false' : 'true') : (invert ? 'true' : 'false'),
+        ...(cleanup && cleanup(value))
       });
     } else {
       modeling.updateModdleProperties(element, businessObject, {
         // eslint-disable-next-line no-nested-ternary
         [property]: value ? (invert ? 'false' : 'true') : (invert ? 'true' : 'false'),
-      });
-    }
-
-    if (helper) {
-      helper.setExtensionProperty(element, modeling, bpmnFactory, {
-        ...(cleanup && cleanup(value))
-      });
-    } else if (cleanupHelper) {
-      cleanupHelper.setExtensionProperty(element, modeling, bpmnFactory, {
-        ...(cleanup && cleanup(value))
-      });
-    } else {
-      modeling.updateModdleProperties(element, businessObject, {
         ...(cleanup && cleanup(value))
       });
     }
