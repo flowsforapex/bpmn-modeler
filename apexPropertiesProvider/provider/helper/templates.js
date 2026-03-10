b2fd25edc914993bc013c78b4cf4da644b3efe7c
Dennis Amthor <dennis.amthor@mt-ag.com>
2026-03-10 08:40:47 +0100

    Reworked context usage in templates

--- apexPropertiesProvider/provider/helper/templates.js
+++ apexPropertiesProvider/provider/helper/templates.js
@@ -7,30 +7,33 @@ import { getBusinessObject, getProperty, updateProperties } from './util';
 import { getContainer, openEditor } from '../plugins/monacoEditor';
 import { OpenDialogLabel } from './OpenDialogLabel';
 
-const genericGetValue = ({helper, element, listElement, property, parent, businessObject} = {}) => {
 
+const genericGetValue = ({ helper, element, businessObject, property, listElement, parent } = {}) => {
   if (helper) {
-    return helper.getProperty({element, listElement, property, parent});
-  } 
+    return helper.getProperty({ element, property, listElement, parent });
+  }
   return getProperty(element, businessObject, property);
 };
 
-const genericSetValue = ({helper, element, listElement, values, parent, businessObject, modeling, bpmnFactory} = {}) => {
-
+const genericSetValue = ({ helper, element, businessObject, values, listElement, parent, context } = {}) => {
+  
+  const { modeling, bpmnFactory } = context;
+  
   if (helper) {
-    helper.setProperty({element, listElement, values, parent, modeling, bpmnFactory});
+    helper.setProperty({ element, values, listElement, parent, modeling, bpmnFactory });
   } else {
     updateProperties(element, businessObject, values, modeling, bpmnFactory);
   }
 };
 
 export function DefaultNumberEntry(props) {
+  
   const { id, element, listElement, label, description, helper, property } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getValue = () => genericGetValue({
@@ -45,12 +48,9 @@ export function DefaultNumberEntry(props) {
     helper,
     element,
     listElement,
-    values: {
-      [property]: value ? String(value) : value,
-    },
+    values: { [property]: value ? String(value) : value },
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   return NumberFieldEntry({
@@ -65,12 +65,12 @@ export function DefaultNumberEntry(props) {
 }
 
 export function DefaultTextFieldEntry(props) {
+  
   const { id, element, listElement, label, description, helper, property, parent } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
   
   const businessObject = listElement || getBusinessObject(element);
 
@@ -87,13 +87,10 @@ export function DefaultTextFieldEntry(props) {
     helper,
     element,
     listElement,
-    values: {
-      [property]: value,
-    },
+    values: { [property]: value },
     parent,
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   return new TextFieldEntry({
@@ -108,45 +105,18 @@ export function DefaultTextFieldEntry(props) {
 }
 
 export function DefaultSelectEntry(props) {
+  
   const { id, element, listElement, label, description, helper, property, defaultValue, options, cleanup, parent } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getValue = () => {
-    let value;
-
-    if (defaultValue) {
-
-      value = genericGetValue({
-        helper,
-        element,
-        listElement,
-        property,
-        parent,
-        businessObject
-      });
-
-      if (!value || !options.some(v => v.value === value)) {
-        genericSetValue({
-          helper,
-          element,
-          listElement,
-          values: {
-            [property]: defaultValue,
-          },
-          parent,
-          businessObject,
-          modeling,
-          bpmnFactory
-        });
-      }
-    }
-
-    return genericGetValue({
+    
+    const value = genericGetValue({
       helper,
       element,
       listElement,
@@ -154,20 +124,32 @@ export function DefaultSelectEntry(props) {
       parent,
       businessObject
     });
+    
+    // set default value if empty or value not in options
+    if (defaultValue && (!value || !options.some(v => v.value === value))) {
+      genericSetValue({
+        helper,
+        element,
+        listElement,
+        values: { [property]: defaultValue },
+        parent,
+        businessObject,
+        context: { modeling, bpmnFactory }
+      });
+      return defaultValue;
+    }
+    
+    return value;
   };
 
   const setValue = value => genericSetValue({
     helper,
     element,
     listElement,
-    values: {
-      [property]: value,
-      ...(cleanup && cleanup(value))
-    },
+    values: { [property]: value, ...(cleanup && cleanup(value)) },
     parent,
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   return new SelectEntry({
@@ -183,12 +165,13 @@ export function DefaultSelectEntry(props) {
 }
 
 export function DefaultSelectEntryAsync(props) {
+  
   const { id, element, listElement, label, description, helper, property, state, needsRefresh } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getOptions = () => {
@@ -200,12 +183,11 @@ export function DefaultSelectEntryAsync(props) {
       property,
       businessObject
     });
-
-    const existing =
-      currValue == null || (state && state.values && state.values.map(e => e.value).includes(currValue));
-
+    
+    const existing = currValue == null || (state && state.values && state.values.map(e => e.value).includes(currValue));
+   
     const result = [];
-
+   
     // only return values if list will not be refreshed
     if (state && !needsRefresh) {
       // only if state loaded, current value is not null and does not exist in list
@@ -224,7 +206,7 @@ export function DefaultSelectEntryAsync(props) {
         );
       }
     }
-
+    
     return result;
   };
 
@@ -240,12 +222,9 @@ export function DefaultSelectEntryAsync(props) {
     helper,
     element,
     listElement,
-    values: {
-      [property]: value,
-    },
+    values: { [property]: value },
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   return new SelectEntry({
@@ -261,63 +240,50 @@ export function DefaultSelectEntryAsync(props) {
 }
 
 export function DefaultToggleSwitchEntry(props) {
+  
   const { id, element, listElement, label, description, helper, property, defaultValue, invert, cleanup, cleanupHelper } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getValue = () => {
-    var value;
-    
-    if (defaultValue) {
-      
-      value = genericGetValue({
-        helper,
-        element,
-        listElement,
-        property,
-        businessObject
-      });
-
-      if (!value) {
-        genericSetValue({
-          helper,
-          element,
-          listElement,
-          values: {
-            [property]: defaultValue,
-          },
-          businessObject,
-          modeling,
-          bpmnFactory
-        });
-      }
-    }
-
-    return genericGetValue({
+    const value = genericGetValue({
       helper,
       element,
       listElement,
       property,
       businessObject
-    }) === (invert ? 'false' : 'true');
-  };
+    });
     
+    // set default value if empty
+    if (defaultValue && !value) {
+      genericSetValue({
+        helper,
+        element,
+        listElement,
+        values: {
+          [property]: defaultValue,
+        },
+        businessObject,
+        context: { modeling, bpmnFactory }
+      });
+      return defaultValue === (invert ? 'false' : 'true');
+    }
+    
+    return value === (invert ? 'false' : 'true');
+  };
+
   const setValue = (value) => {
     genericSetValue({
       helper,
       element,
       listElement,
-      values: {
-        // eslint-disable-next-line no-nested-ternary
-        [property]: value ? (invert ? 'false' : 'true') : (invert ? 'true' : 'false'),
-      },
+      values: { [property]: value ? (invert ? 'false' : 'true') : (invert ? 'true' : 'false') },
       businessObject,
-      modeling,
-      bpmnFactory
+      context: { modeling, bpmnFactory }
     });
 
     // special cleanup for toggle switch entries
@@ -325,9 +291,7 @@ export function DefaultToggleSwitchEntry(props) {
       helper.setProperty({
         element,
         listElement,
-        values: {
-          ...(cleanup && cleanup(value))
-        },
+        values: { ...(cleanup && cleanup(value)) },
         businessObject,
         modeling,
         bpmnFactory
@@ -336,17 +300,13 @@ export function DefaultToggleSwitchEntry(props) {
       cleanupHelper.setProperty({
         element,
         listElement,
-        values: {
-          ...(cleanup && cleanup(value))
-        },
+        values: { ...(cleanup && cleanup(value)) },
         businessObject,
         modeling,
         bpmnFactory
       });
     } else {
-      modeling.updateModdleProperties(element, businessObject, {
-        ...(cleanup && cleanup(value))
-      });
+      modeling.updateModdleProperties(element, businessObject, { ...(cleanup && cleanup(value)) });
     }
   };
 
@@ -362,12 +322,13 @@ export function DefaultToggleSwitchEntry(props) {
 }
 
 export function DefaultTextAreaEntry(props) {
+  
   const { id, element, listElement, label, description, helper, property, parent } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getValue = () => genericGetValue({
@@ -383,13 +344,10 @@ export function DefaultTextAreaEntry(props) {
     helper,
     element,
     listElement,
-    values: {
-      [property]: value,
-    },
+    values: { [property]: value },
     parent,
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   return new TextAreaEntry({
@@ -404,13 +362,14 @@ export function DefaultTextAreaEntry(props) {
 }
 
 export function DefaultTextAreaEntryWithEditor(props) {
+  
   const { id, element, listElement, label, description, helper, property, language, type, parent } = props;
-
+  
   const modeling = useService('modeling');
   const debounce = useService('debounceInput');
   const bpmnFactory = useService('bpmnFactory');
   const translate = useService('translate');
-
+  
   const businessObject = listElement || getBusinessObject(element);
 
   const getValue = () => genericGetValue({
@@ -426,13 +385,10 @@ export function DefaultTextAreaEntryWithEditor(props) {
     helper,
     element,
     listElement,
-    values: {
-      [property]: value,
-    },
+    values: { [property]: value },
     parent,
     businessObject,
-    modeling,
-    bpmnFactory
+    context: { modeling, bpmnFactory }
   });
 
   const labelWithIcon =
@@ -449,13 +405,10 @@ export function DefaultTextAreaEntryWithEditor(props) {
         helper,
         element,
         listElement,
-        values: {
-          [property]: text,
-        },
+        values: { [property]: text },
         parent,
         businessObject,
-        modeling,
-        bpmnFactory
+        context: { modeling, bpmnFactory }
       });
       openEditor(
         getProperty,
