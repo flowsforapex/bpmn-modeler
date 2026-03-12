9ff2b8aa1180ae7693bc82a5cafb0ccb41a6269d
Dennis Amthor <dennis.amthor@mt-ag.com>
2026-03-12 11:56:24 +0100

    Outsourced section configuration

--- apexPropertiesProvider/provider/apexPropertiesProvider.js
+++ apexPropertiesProvider/provider/apexPropertiesProvider.js
@@ -1,43 +1,29 @@
-import { is } from 'bpmn-js/lib/util/ModelUtil';
+
+import { setDefaultSequence } from './parts/sequenceFlow/SequenceFlowProps';
+
+import {
+  businessRuleTaskSections,
+  callActivitySections,
+  customExtensionSections,
+  endEventSections,
+  eventSections,
+  gatewaySections,
+  laneSections,
+  manualTaskSections,
+  processSections,
+  receiveTaskSections,
+  scriptTaskSections,
+  sendTaskSections,
+  sequenceFlowSections,
+  serviceTaskSections,
+  startEventSections,
+  subProcessSections,
+  taskSections,
+  userTaskSections
+} from './section-config';
+
+import { is, isAny } from 'bpmn-js/lib/features/modeling/util/ModelingUtil';
 import { getBusinessObject } from './helper/util';
-import ExecutePlsqlProps from './parts/executePlsql/ExecutePlsqlProps';
-import ProcVarGroup from './parts/processVariables/ProcVarGroup';
-import ApexPageProps from './parts/userTask/ApexPageProps';
-import ApexApprovalProps from './parts/userTask/ApprovalTaskProps';
-import ApexSimpleFormProps from './parts/userTask/SimpleFormProps';
-
-import TaskTypeProps from './parts/task/TaskTypeProps';
-import CustomTimerProps from './parts/timer/CustomTimerProps';
-
-import AssignmentProps from './parts/userTask/AssignmentProps';
-
-import ExecutionProps from './parts/process/ExecutionProps';
-import ProcessProps from './parts/process/ProcessProps';
-
-import RoleProps from './parts/lane/RoleProps';
-import SchedulingProps from './parts/scheduling/SchedulingProps';
-
-import CallActivityProps from './parts/callActivity/CallActivityProps';
-import StarterProps from './parts/process/StarterProps';
-
-import TerminateEventProps from './parts/events/TerminateEventProps';
-import BackgroundTaskSessionProps from './parts/process/BackgroundTaskSessionProps';
-import SequenceFlowProps, { setDefaultSequence } from './parts/sequenceFlow/SequenceFlowProps';
-
-import ApexAIGenerationProps from './parts/serviceTask/ApexAIGenerationProps';
-import SendMailProps from './parts/serviceTask/SendMailProps';
-
-import CustomExtensionProps from './parts/CustomExtensionProps';
-import EventTypeProps from './parts/events/EventTypeProps';
-import SimpleMessageProps from './parts/message/SimpleMessageProps';
-
-import MultiInstanceLoopProps from './parts/multiInstanceLoop/MultiInstanceLoopProps';
-
-import InOutParamGroup from './parts/inOutParameters/InOutParamGroup';
-import { AdHocSubProcessProps, CompletionProps, DetailPageProps, StartingProps, VisibilityProps } from './parts/subprocess/AdHocSubprocessProps';
-import TaskProps from './parts/task/TaskProps';
-
-var ModelingUtil = require('bpmn-js/lib/features/modeling/util/ModelingUtil');
 
 const LOW_PRIORITY = 500;
 export default function apexPropertiesProvider(
@@ -62,197 +48,71 @@ export default function apexPropertiesProvider(
     setDefaultSequence(event.context.connection, modeling);
   });
 
-  this.getGroups = function (element) {
-    return function (groups) {
-      const newGroups = [];
+  this.getGroups = function(element) {
+    return function(groups) {
+      
+      // helper function to add a section of properties
+      const addSection = (config) => {
 
-      const addSection = (id, labelKey, props) => {
-        const section = {
-          id: id,
-          label: labelKey ? translate(labelKey) : null,
-          entries: props({ element, injector, translate })
-        };
+        config.forEach(c => {
 
-        newGroups.push(section);
+          // if element not in exclude list
+          if (!c.exclude || !isAny(element, c.exclude)) {
+
+            // get entries
+            const entries = c.props({ element, injector, translate });
+            
+            // if entries existing
+            if (entries.length > 0) {
+
+              // append to general group
+              if (c.id === 'general') {
+                const generalGroup = groups.find(g => g.id === 'general');
+                generalGroup.entries = generalGroup.entries.concat(entries);
+              }
+              // create new group
+              else {
+                const label = typeof c.label === 'function' ? translate(c.label(element)) : c.label ? translate(c.label) : null;
+                
+                groups.push({ id: c.id, label: label, entries: entries });
+              }
+            }
+          }
+        })
       };
 
-      const businessObject = getBusinessObject(element);
-      
-      let multiInstanceLoopHeading;
-      
-      if (businessObject.loopCharacteristics) {
-        if (is(businessObject.loopCharacteristics, 'bpmn:MultiInstanceLoopCharacteristics')) multiInstanceLoopHeading = 'Multi-Instance';
-        if (is(businessObject.loopCharacteristics, 'bpmn:StandardLoopCharacteristics')) multiInstanceLoopHeading = 'Loop';
-      }
+      // mapping object between element types and property configurations
+      const sectionConfigs = {
+        'bpmn:Task': taskSections,
+        'bpmn:UserTask': userTaskSections,
+        'bpmn:ScriptTask': scriptTaskSections,
+        'bpmn:ServiceTask': serviceTaskSections,
+        'bpmn:BusinessRuleTask': businessRuleTaskSections,
+        'bpmn:SendTask': sendTaskSections,
+        'bpmn:ReceiveTask': receiveTaskSections,
+        'bpmn:ManualTask': manualTaskSections,
+        'bpmn:CallActivity': callActivitySections,
+        'bpmn:Process': processSections,
+        'bpmn:SubProcess': subProcessSections,
+        'bpmn:IntermediateCatchEvent': eventSections,
+        'bpmn:IntermediateThrowEvent': eventSections,
+        'bpmn:BoundaryEvent': eventSections,
+        'bpmn:Lane': laneSections,
+        'bpmn:Gateway': gatewaySections,
+        'bpmn:SequenceFlow': sequenceFlowSections,
+        'bpmn:StartEvent': startEventSections,
+        'bpmn:EndEvent': endEventSections,
+      };
 
-      // task
-      if (
-        is(element, 'bpmn:Task') &&
-        !ModelingUtil.isAny(element, ['bpmn:UserTask', 'bpmn:ScriptTask', 'bpmn:ServiceTask', 'bpmn:BusinessRuleTask', 'bpmn:SendTask', 'bpmn:ReceiveTask', 'bpmn:ManualTask'])
-      ) {
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-        
-        addSection('parameters', 'Parameters', InOutParamGroup);
-
-        const generalGroup = groups.find(g => g.id === 'general');
-        generalGroup.entries = generalGroup.entries.concat(TaskProps({element, injector, translate}));
-      }
-
-      //TODO add subject to usertask & adhocsp
-
-      // userTask
-      if (is(element, 'bpmn:UserTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('apexPage', 'APEX Page', ApexPageProps);
-        addSection('apexApproval', 'APEX Human Task', ApexApprovalProps);
-        addSection('simpleForm', 'APEX Simple Form', ApexSimpleFormProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('assignment', 'Assignment', AssignmentProps);
-        addSection('scheduling', 'Scheduling', SchedulingProps);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // scriptTask
-      if (is(element, 'bpmn:ScriptTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('executePlsql', 'PL/SQL', ExecutePlsqlProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // serviceTask
-      if (is(element, 'bpmn:ServiceTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('executePlsql', 'PL/SQL', ExecutePlsqlProps);
-        addSection('sendMail', 'Mail', SendMailProps);
-        addSection('apexAIGeneration', 'APEX AI Generation', ApexAIGenerationProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // businessRuleTask
-      if (is(element, 'bpmn:BusinessRuleTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('executePlsql', 'PL/SQL', ExecutePlsqlProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // sendTask
-      if (is(element, 'bpmn:SendTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('executePlsql', 'PL/SQL', ExecutePlsqlProps);
-        addSection('simpleMessage', 'Simple Message', SimpleMessageProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // receiveTask
-      if (is(element, 'bpmn:ReceiveTask')) {
-        addSection('taskType', 'Task Type', TaskTypeProps);
-        addSection('executePlsql', 'PL/SQL', ExecutePlsqlProps);
-        addSection('simpleMessage', 'Simple Message', SimpleMessageProps);
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // manualTask
-      if (is(element, 'bpmn:ManualTask')) {
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-      }
-
-      // callActivity
-      if (is(element, 'bpmn:CallActivity')) {
-        addSection('callActivity', 'Called Diagram', CallActivityProps);
-        addSection('procVars', 'In/Out Mapping', ProcVarGroup);
-      }
-
-      // process
-      if (is(element, 'bpmn:Process') || is(getBusinessObject(element), 'bpmn:Process')) {
-        addSection('execution', 'Execution', ExecutionProps);
-        addSection('procVars', 'In/Out Variables', ProcVarGroup);
-        addSection('starter', 'Potential Starters', StarterProps);
-        addSection('backgroundTaskSession', 'Background Task Session', BackgroundTaskSessionProps);
-        addSection('scheduling', 'Scheduling', SchedulingProps);
-
-        const generalGroup = groups.find(g => g.id === 'general');
-        generalGroup.entries = generalGroup.entries.concat(ProcessProps({element, injector, translate}));
-      }
-
-      // subprocess
-      if (is(element, 'bpmn:SubProcess')) {
-        addSection('loop', multiInstanceLoopHeading, MultiInstanceLoopProps);
-        addSection('starting', 'Starting Activities', StartingProps);
-        addSection('completion', 'Completion Condition', CompletionProps);
-        addSection('visibility', 'Visibility', VisibilityProps);
-        addSection('detailPage', 'Detail Page', DetailPageProps);
-        addSection('assignment', 'Assignment', AssignmentProps);
-        addSection('scheduling', 'Scheduling', SchedulingProps);
-
-        const generalGroup = groups.find(g => g.id === 'general');
-        generalGroup.entries = generalGroup.entries.concat(AdHocSubProcessProps({element, injector, translate}));
-      }
-
-      // add the message event props
-      if (is(element, 'bpmn:IntermediateCatchEvent') || is(element, 'bpmn:IntermediateThrowEvent') || is(element, 'bpmn:BoundaryEvent')) {
-        addSection('eventType', 'Event Type', EventTypeProps);
-        addSection('simpleMessage', 'Simple Message', SimpleMessageProps);
-      }
-
-      // lane
-      if (is(element, 'bpmn:Lane')) {
-        addSection('role', 'APEX Role', RoleProps);
-      }
-
-      // gateways
-      if (is(element, 'bpmn:Gateway')) {
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-      }
-
-      // add the routing expression section
-      if (is(element, 'bpmn:SequenceFlow')) {
-        addSection('condition', 'Condition', SequenceFlowProps);
-      }
-
-      // add the custom timer section
-      addSection('customTimer', 'Timer', CustomTimerProps);
-      
-      // add start event sections
-      if (is(element, 'bpmn:StartEvent')) {
-        addSection('eventType', 'Event Type', EventTypeProps);
-        addSection('simpleMessage', 'Simple Message', SimpleMessageProps);
-        addSection('parameters', 'Parameters', InOutParamGroup);
-      }
-      
-      // add terminate event section
-      if (is(element, 'bpmn:EndEvent')) {
-        addSection('eventType', 'Event Type', EventTypeProps);
-        addSection('simpleMessage', 'Simple Message', SimpleMessageProps);
-        addSection('customTerminate', 'Details', TerminateEventProps);
-      }
-
-      // add event proc var section
-      if (is(element, 'bpmn:Event')) {
-        addSection('procVars', 'Variable Expressions', ProcVarGroup);
-      }
+      // loop over mapping and add all relevant sections
+      Object.entries(sectionConfigs)
+      .filter(([type, _]) => is(element, type) || is(getBusinessObject(element), type))
+      .forEach(([, config]) => addSection(config));
 
       // add custom section
-      if (showCustomExtensions) {
-        addSection('custom', 'Custom', CustomExtensionProps);
-      }
-      
-      // filter: add all non-empty groups
-      newGroups.forEach((g) => {
-        if (typeof g.entries !== 'undefined' && g.entries.length > 0) groups.push(g);
-      });
+      if (showCustomExtensions) addSection(customExtensionSections);
 
+      // remove default bpmn groups
       const removeGroups = ['timer', 'message', 'multiInstance', 'adHocCompletion'];
       
       const filteredGroups = groups.filter(g => !removeGroups.includes(g.id));
---
+++ apexPropertiesProvider/provider/section-config.js
@@ -0,0 +1,212 @@
+import { is } from 'bpmn-js/lib/util/ModelUtil';
+import { getBusinessObject } from './helper/util';
+
+// callActivity
+import CallActivityProps from './parts/callActivity/CallActivityProps';
+
+// events
+import EventTypeProps from './parts/events/EventTypeProps';
+import TerminateEventProps from './parts/events/TerminateEventProps';
+
+// executePlsql
+import ExecutePlsqlProps from './parts/executePlsql/ExecutePlsqlProps';
+
+// inOutParameters
+import InOutParamGroup from './parts/inOutParameters/InOutParamGroup';
+
+// lane
+import RoleProps from './parts/lane/RoleProps';
+
+// message
+import SimpleMessageProps from './parts/message/SimpleMessageProps';
+
+// multiInstanceLoop
+import MultiInstanceLoopProps from './parts/multiInstanceLoop/MultiInstanceLoopProps';
+
+// process
+import BackgroundTaskSessionProps from './parts/process/BackgroundTaskSessionProps';
+import ExecutionProps from './parts/process/ExecutionProps';
+import ProcessProps from './parts/process/ProcessProps';
+import StarterProps from './parts/process/StarterProps';
+
+// processVariables
+import ProcVarGroup from './parts/processVariables/ProcVarGroup';
+
+// scheduling
+import SchedulingProps from './parts/scheduling/SchedulingProps';
+
+// sequenceFlow
+import SequenceFlowProps from './parts/sequenceFlow/SequenceFlowProps';
+
+// serviceTask
+import ApexAIGenerationProps from './parts/serviceTask/ApexAIGenerationProps';
+import SendMailProps from './parts/serviceTask/SendMailProps';
+
+// subprocess
+import { AdHocSubProcessProps, CompletionProps, DetailPageProps, StartingProps, VisibilityProps } from './parts/subprocess/AdHocSubprocessProps';
+
+// task
+import TaskProps from './parts/task/TaskProps';
+import TaskTypeProps from './parts/task/TaskTypeProps';
+
+// timer
+import CustomTimerProps from './parts/timer/CustomTimerProps';
+
+// userTask
+import ApexPageProps from './parts/userTask/ApexPageProps';
+import ApprovalProps from './parts/userTask/ApprovalTaskProps';
+import AssignmentProps from './parts/userTask/AssignmentProps';
+import SimpleFormProps from './parts/userTask/SimpleFormProps';
+
+// custom
+import CustomExtensionProps from './parts/CustomExtensionProps';
+
+// helper for dynamic loop heading
+function multiInstanceHeading(element) {
+  const bo = getBusinessObject(element);
+  if (!bo.loopCharacteristics) { return null; }
+  if (is(bo.loopCharacteristics, 'bpmn:MultiInstanceLoopCharacteristics')) return 'Multi-Instance';
+  if (is(bo.loopCharacteristics, 'bpmn:StandardLoopCharacteristics')) return 'Loop';
+  return null;
+}
+
+const taskSectionsExclude = [
+  'bpmn:UserTask',
+  'bpmn:ScriptTask',
+  'bpmn:ServiceTask',
+  'bpmn:BusinessRuleTask',
+  'bpmn:SendTask',
+  'bpmn:ReceiveTask',
+  'bpmn:ManualTask'
+]
+
+export const taskSections = [
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup, exclude: taskSectionsExclude },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup, exclude: taskSectionsExclude },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps, exclude: taskSectionsExclude },
+  { id: 'general', props: TaskProps}
+];
+
+export const userTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'apexPage', label: 'APEX Page', props: ApexPageProps },
+  { id: 'apexApproval', label: 'APEX Human Task', props: ApprovalProps },
+  { id: 'simpleForm', label: 'APEX Simple Form', props: SimpleFormProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'assignment', label: 'Assignment', props: AssignmentProps },
+  { id: 'scheduling', label: 'Scheduling', props: SchedulingProps },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const scriptTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'executePlsql', label: 'PL/SQL', props: ExecutePlsqlProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const serviceTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'executePlsql', label: 'PL/SQL', props: ExecutePlsqlProps },
+  { id: 'sendMail', label: 'Mail', props: SendMailProps },
+  { id: 'apexAIGeneration', label: 'APEX AI Generation', props: ApexAIGenerationProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const businessRuleTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'executePlsql', label: 'PL/SQL', props: ExecutePlsqlProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const sendTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'executePlsql', label: 'PL/SQL', props: ExecutePlsqlProps },
+  { id: 'simpleMessage', label: 'Simple Message', props: SimpleMessageProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const receiveTaskSections = [
+  { id: 'taskType', label: 'Task Type', props: TaskTypeProps },
+  { id: 'executePlsql', label: 'PL/SQL', props: ExecutePlsqlProps },
+  { id: 'simpleMessage', label: 'Simple Message', props: SimpleMessageProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup },
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const manualTaskSections = [
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps }
+];
+
+export const callActivitySections = [
+  { id: 'callActivity', label: 'Called Diagram', props: CallActivityProps },
+  { id: 'procVars', label: 'In/Out Mapping', props: ProcVarGroup }
+];
+
+export const processSections = [
+  { id: 'execution', label: 'Execution', props: ExecutionProps },
+  { id: 'procVars', label: 'In/Out Variables', props: ProcVarGroup },
+  { id: 'starter', label: 'Potential Starters', props: StarterProps },
+  { id: 'backgroundTaskSession', label: 'Background Task Session', props: BackgroundTaskSessionProps },
+  { id: 'scheduling', label: 'Scheduling', props: SchedulingProps },
+  { id: 'general', props: ProcessProps}
+];
+
+export const subProcessSections = [
+  { id: 'loop', label: multiInstanceHeading, props: MultiInstanceLoopProps },
+  { id: 'starting', label: 'Starting Activities', props: StartingProps },
+  { id: 'completion', label: 'Completion Condition', props: CompletionProps },
+  { id: 'visibility', label: 'Visibility', props: VisibilityProps },
+  { id: 'detailPage', label: 'Detail Page', props: DetailPageProps },
+  { id: 'assignment', label: 'Assignment', props: AssignmentProps },
+  { id: 'scheduling', label: 'Scheduling', props: SchedulingProps },
+  { id: 'general', props: AdHocSubProcessProps}
+];
+
+export const eventSections = [
+  { id: 'eventType', label: 'Event Type', props: EventTypeProps },
+  { id: 'customTimer', label: 'Timer', props: CustomTimerProps },
+  { id: 'simpleMessage', label: 'Simple Message', props: SimpleMessageProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup }
+];
+
+export const laneSections = [
+  { id: 'role', label: 'APEX Role', props: RoleProps }
+];
+
+export const gatewaySections = [
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup }
+];
+
+export const sequenceFlowSections = [
+  { id: 'condition', label: 'Condition', props: SequenceFlowProps }
+];
+
+export const startEventSections = [
+  { id: 'eventType', label: 'Event Type', props: EventTypeProps },
+  { id: 'customTimer', label: 'Timer', props: CustomTimerProps },
+  { id: 'simpleMessage', label: 'Simple Message', props: SimpleMessageProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup },
+  { id: 'parameters', label: 'Parameters', props: InOutParamGroup }
+];
+
+export const endEventSections = [
+  { id: 'eventType', label: 'Event Type', props: EventTypeProps },
+  { id: 'customTimer', label: 'Timer', props: CustomTimerProps },
+  { id: 'simpleMessage', label: 'Simple Message', props: SimpleMessageProps },
+  { id: 'customTerminate', label: 'Details', props: TerminateEventProps },
+  { id: 'procVars', label: 'Variable Expressions', props: ProcVarGroup }
+];
+
+export const customExtensionSections = [
+  { id: 'custom', label: 'Custom', props: CustomExtensionProps }
+];
